9a2d0d5fd46349d22510b2c0820fc3ea889f119d
takuya-sangil <jayloveswonyoung@gmail.com>
2024-05-06 00:01:27 +0800

    added test debug file

---
+++ BasicTests/DebugFizzBuzz.py
@@ -0,0 +1,21 @@
+# Test Fizz buzz debug
+
+def debug_fizz(limit):
+  
+  for i in range(1, limit + 1):
+    
+    if i % 3 == 0 and i % 5 == 0:
+      print("Fizz Buzz!")
+      
+    elif i % 3 == 0:
+      print("Fizz!")
+      
+    elif i % 5 == 0:
+      print("Buzz!")
+      
+    else:
+      print(i)
+      
+debug_fizz(100)
+      
+      
