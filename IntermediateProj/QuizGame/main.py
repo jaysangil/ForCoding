943de379a50f1b21c7166246c5a741133e187e56
takuya-sangil <jayloveswonyoung@gmail.com>
2024-05-26 00:05:37 +0800

    Completed the QuizGame project

--- IntermediateProj/QuizGame/main.py
+++ IntermediateProj/QuizGame/main.py
@@ -13,3 +13,6 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions(): #if quiz still has questions remaining:
   quiz.next_question()
+  
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz.score}/{len(question_bank)} ")
