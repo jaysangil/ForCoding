19315166214aa5bec27aaf2a7e06e7773823b7a8
takuya-sangil <jayloveswonyoung@gmail.com>
2024-06-10 01:06:39 +0800

    Added detection collision and scoreboard

--- IntermediateProj/PongGame/main.py
+++ IntermediateProj/PongGame/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -10,12 +11,10 @@ screen.setup(width=800, height=700)
 screen.title("Pong Game")
 screen.tracer(0)
 
-
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -25,14 +24,26 @@ screen.onkey(l_paddle.go_down, "s")
 
 is_game_on = True
 while is_game_on:
-  time.sleep(0.1)
-  screen.update()
-  ball.move()
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
   
-  #detect colision with the wall
-  if ball.ycor() > 280 or ball.ycor() < -280:
-    ball.bounce()
-
+    # Detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    
+    # Detect collision with paddles
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+    
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    
+    # Detect L paddle misses
+    if ball.xcor() < -380:  # Corrected logical error here
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
-
