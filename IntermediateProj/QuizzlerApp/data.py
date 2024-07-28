a81574e2f72cde7fd9d52789e52026c27f0cd0a3
takuya-sangil <jayloveswonyoung@gmail.com>
2024-07-28 22:20:43 +0800

    Added GUI

--- IntermediateProj/QuizzlerApp/data.py
+++ IntermediateProj/QuizzlerApp/data.py
@@ -1,13 +1,14 @@
 import requests
 parameters = {
     "amount": 10,
-    "type": "boolean"
+    "type": "boolean",
+    "category": 18,
 }
 
 response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
-question_data = print(data["results"])
+question_data = (data["results"])
 
 
 
